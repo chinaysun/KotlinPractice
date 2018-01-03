5b63cecaaf81684a374066876cfdbfcf1dc42a63
chinaysun <ausysun@gmail.com>
2018-01-03 16:07:53 +1100

    Fragment

---
+++ RxToDoListApp/app/src/main/java/com/example/suny/rxtodolistapp/DetailScreen/TaskDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.example.suny.rxtodolistapp.DetailScreen
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.suny.rxtodolistapp.R
+
+class TaskDetailActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_detail)
+    }
+}
